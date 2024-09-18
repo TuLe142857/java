1e08eaca3eefa6c592450f1455707d1c0f1eb674
TuLe <tuleaibk@gmail.com>
2024-09-18 12:18:18 +0700

    luu o day chieu TH lam tiep

---
+++ codeptit/IntSet.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class IntSet {
+	//MAIN
+	public static void main(String []args){
+		Scanner in = new Scanner(System.in);
+
+	}
+
+	//ATTRIBUTES & METHODS
+
+	private ArrayList<Integer> list;
+
+	public IntSet(){
+		list = new ArrayList<>();
+	}
+
+	//size, add, get, set, hop, toString
+	public int size(){
+		return list.size();
+	}
+
+	public void add(int value){
+		//empty
+
+
+		//them cuoi
+
+		//them giua(hoac dau)
+	}
+}
